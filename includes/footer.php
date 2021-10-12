a1688dfb12973a688d96603f2a8a1114b58d07de
FahimWeblogicAndCyberTechnologies <weblogicfahim@gmail.com>
2021-10-12 04:43:35 -0400

    cvms

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+                            <div class="col-md-12">
+                                <div class="copyright">
+                                    <p>MPFVMS 2021. All rights reserved.</p>
+                                </div>
+                            </div>
+                        </div>
